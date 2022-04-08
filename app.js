5c7051986f685c314a86775bb8c052f2d26de274
Himanshu <himanshum50360@gmail.com>
2022-04-08 20:44:42 +0530

    fixed google callback url

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/compose",
+    callbackURL: "https://nodeblog24.herokuapp.com/auth/google/compose",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
